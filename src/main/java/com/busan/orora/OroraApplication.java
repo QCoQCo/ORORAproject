b37d7deb9f40bc118bb672a6894455757bc27b0c
QCoQCo <kbw7700@gmail.com>
2025-12-31 13:58:02 +0900

    gradle error

--- src/main/java/com/busan/orora/OroraApplication.java
+++ src/main/java/com/busan/orora/OroraApplication.java
@@ -8,14 +8,12 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class OroraApplication {
 
 	public static void main(String[] args) {
-		// .env 파일 로드
 		try {
 			Dotenv dotenv = Dotenv.configure()
 					.directory("./")
 					.ignoreIfMissing()
 					.load();
-			
-			// 환경 변수 설정
+
 			System.setProperty("DB_HOST", dotenv.get("DB_HOST", "localhost"));
 			System.setProperty("DB_PORT", dotenv.get("DB_PORT", "3306"));
 			System.setProperty("DB_NAME", dotenv.get("DB_NAME", "arata_busan"));
@@ -23,9 +21,8 @@ public class OroraApplication {
 			System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD", ""));
 		} catch (Exception e) {
 			System.err.println(".env 파일 로드 실패: " + e.getMessage());
-			// .env 파일이 없어도 기본값으로 실행
 		}
-		
+
 		SpringApplication.run(OroraApplication.class, args);
 	}
 
